ef02ed09f4576fbadcf47f7241ae8867eaf72c7e
mariamerino99 <m.merinop@alumnos.upm.es>
2021-05-10 18:00:29 +0200

    Personal Form

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,62 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+from typing import Text, List, Optional
+from rasa_sdk.forms import FormAction
+from rasa_sdk.forms import FormValidationAction
+
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+
+
+class ValidatePersonalForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_personal_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+        return ["name", "age", "antecedentes"]
+    def validate_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate name value."""
+
+        # validation succeeded, set the value of the "cuisine" slot to value
+        return {"name": slot_value}
+
+    def validate_age(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
+            """Validate age value."""
+            return {"age": slot_value}
+
+    def validate_antecedentes(
+                self,
+                slot_value: Any,
+                dispatcher: CollectingDispatcher,
+                tracker: Tracker,
+                domain: DomainDict,
+    ) -> Dict[Text, Any]:
+                """Validate antecedentes value."""
+                return {"antecedentes": slot_value}
+
+
+
+
+
+
