9b6eba8141edc0b47793cd31af9dd803bea80866
mariamerino99 <m.merinop@alumnos.upm.es>
2021-05-17 11:40:39 +0200

    prob inicial acabada

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -27,57 +27,90 @@
 #         return []
 
 
-from typing import Text, List, Optional
+from typing import Text, List, Optional, Union
 from rasa_sdk.forms import FormAction
 from rasa_sdk.forms import FormValidationAction
 
-from typing import Text, List, Any, Dict
 
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
-
-
-class ValidatePersonalForm(FormValidationAction):
+class SetAntecedentesSi(Action):
+    # return the name of the action
     def name(self) -> Text:
-        return "validate_personal_form"
+        return "set_antecedentes_si"
 
-    @staticmethod
-    def required_slots(tracker: Tracker) -> List[Text]:
-        """A list of required slots that the form has to fill"""
-        return ["name", "age", "antecedentes"]
-    def validate_name(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        """Validate name value."""
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [[SlotSet("antecedentes", "si")]]
 
-        # validation succeeded, set the value of the "cuisine" slot to value
-        return {"name": slot_value}
+class SetAntecedentesNo(Action):
+    # return the name of the action
+    def name(self) -> Text:
+        return "set_antecedentes_no"
 
-    def validate_age(
-            self,
-            slot_value: Any,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-            """Validate age value."""
-            return {"age": slot_value}
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        res = 'no'
 
-    def validate_antecedentes(
-                self,
-                slot_value: Any,
-                dispatcher: CollectingDispatcher,
-                tracker: Tracker,
-                domain: DomainDict,
-    ) -> Dict[Text, Any]:
-                """Validate antecedentes value."""
-                return {"antecedentes": slot_value}
+        return [SlotSet("antecedentes", "no")]
+
+class SetResultadoDefs(Action):
+    # return the name of the action
+    def name(self) -> Text:
+        return "set_resultado_defs"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        res = tracker.get("res_def")
+        res += 1
+
+        return [SlotSet("res_def", res)]
+
+class ProbabilidadInicial(Action):
+    # return the name of the action
+    def name(self) -> Text:
+        return "probabilidad_inicial"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # get all the entities extracted by rasa
+        name= tracker.slots.get("name")
+        ages= tracker.slots.get("age")
+        antecedentes= tracker.slots.get("antecedentes")
+        prob = 2
+
+        # sanity check to ensure that it was filled by rasa
+        if name:
+            dispatcher.utter_message("Hemos llegado hasta NOMBRE", name)
+        if ages:
+            dispatcher.utter_message("Hemos llegado hasta EDAD", ages)
+        if antecedentes:
+            dispatcher.utter_message("Hemos llegado hasta ANTEC", antecedentes)
+
+        age = int(ages)
+        if(age < 65 and antecedentes):
+            prob = 0
+        elif(age > 65 and age < 75 ):
+            if(antecedentes == "no"):
+                prob = 1
+            elif(antecedentes == "si"):
+                prob = 2
+        elif (age > 75 ):
+            if antecedentes == "no":
+                prob = 2
+            elif antecedentes == "si":
+                prob = 3
+
+
+        print(prob)
+        return [SlotSet("prob", prob)]
+
+
+
+
 
 
 
